060d01e79878fc91506902584b9dc033cabcec8b
juanMS <285juanda@gmail.com>
2023-05-29 00:23:24 -0500

    Se agrega validaciones a las variables

--- src/main/java/unisabana/edu/co/SigaBack/Profesor.java
+++ src/main/java/unisabana/edu/co/SigaBack/Profesor.java
@@ -3,10 +3,20 @@ package unisabana.edu.co.SigaBack;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @Data
 @AllArgsConstructor
 public class Profesor {
+
+    @Max(10)
+    @Min(3)
     private String nombre;
+    @Max(10)
+    @Min(3)
     private int id;
+    @Max(10)
+    @Min(3)
     private String facultad;
 }
